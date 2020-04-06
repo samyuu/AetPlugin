ce2c401ba67d8f89511b005e033afaba669e2278
samyuu <samyuu@users.noreply.github.com>
2020-04-06 03:33:46 +0200

    AetPlugin update and clean up RegisterAetSetFileType

--- src/AetPlugin.cpp
+++ src/AetPlugin.cpp
@@ -10,6 +10,12 @@ namespace AetPlugin
 
 	namespace
 	{
+		constexpr std::array AetSetFileExtensions =
+		{
+			std::array { 'b', 'i', 'n' },
+			std::array { 'a', 'e', 'c' },
+		};
+
 		A_Err AEGP_FileVerifyCallbackHandler(const A_UTF16Char* filePath, AE_FIM_Refcon refcon, A_Boolean* a_canImport)
 		{
 			const auto verifyResult = AetImporter::VerifyAetSetImportable(AEUtil::WCast(filePath));
@@ -32,45 +38,43 @@ namespace AetPlugin
 			return error;
 		}
 
-		A_Err RegisterAetFileType()
+		A_Err RegisterAetSetFileType()
 		{
 			const auto suites = AEGP_SuiteHandler(GlobalBasicPicaSuite);
-			// suites.RegisterSuite5()->AEGP_RegisterUpdateMenuHook();
 
-			static constexpr std::array extensionsToRegister =
+			std::array<AEIO_FileKind, 1> fileTypes = {};
+			for (auto& fileType : fileTypes)
 			{
-				std::array { 'b', 'i', 'n' },
-				std::array { 'a', 'e', 'c' },
-			};
+				fileType.mac.type = AEIO_FileType_GENERIC;
+				fileType.mac.creator = AEIO_ANY_CREATOR;
+			}
+
+			std::array<AEIO_FileKind, AetSetFileExtensions.size()> fileExtensions = {};
+			for (size_t i = 0; i < AetSetFileExtensions.size(); i++)
+			{
+				fileExtensions[i].ext.pad = '.';
+				fileExtensions[i].ext.extension[0] = AetSetFileExtensions[i][0];
+				fileExtensions[i].ext.extension[1] = AetSetFileExtensions[i][1];
+				fileExtensions[i].ext.extension[2] = AetSetFileExtensions[i][2];
+			}
+
+			AE_FIM_ImportFlavorRef importFlavorRef = AE_FIM_ImportFlavorRef_NONE;
 
 			A_Err err = A_Err_NONE;
-			for (const auto& extension : extensionsToRegister)
-			{
-				AEIO_FileKind kind = {};
-				std::memcpy(&kind.mac.type, "aet", 3);
-				kind.mac.creator = AEIO_ANY_CREATOR;
+			ERR(suites.FIMSuite3()->AEGP_RegisterImportFlavor("Project DIVA AetSet", &importFlavorRef));
+			ERR(suites.FIMSuite3()->AEGP_RegisterImportFlavorFileTypes(importFlavorRef,
+				static_cast<A_long>(fileTypes.size()), fileTypes.data(),
+				static_cast<A_long>(fileExtensions.size()), fileExtensions.data()));
 
-				AEIO_FileKind fileKind = {};
-				fileKind.ext.pad = '.';
-				fileKind.ext.extension[0] = extension[0];
-				fileKind.ext.extension[1] = extension[1];
-				fileKind.ext.extension[2] = extension[2];
+			if (err || importFlavorRef == AE_FIM_ImportFlavorRef_NONE)
+				return err;
 
-				AE_FIM_ImportFlavorRef importFlavorRef = AE_FIM_ImportFlavorRef_NONE;
+			AE_FIM_ImportCallbacks importCallbacks = {};
+			importCallbacks.refcon = nullptr;
+			importCallbacks.import_cb = AEGP_FileImportCallbackHandler;
+			importCallbacks.verify_cb = AEGP_FileVerifyCallbackHandler;
 
-				ERR(suites.FIMSuite3()->AEGP_RegisterImportFlavor("XXX Project DIVA Aet", &importFlavorRef));
-				ERR(suites.FIMSuite3()->AEGP_RegisterImportFlavorFileTypes(importFlavorRef, 1, &kind, 1, &fileKind));
-
-				if (err || importFlavorRef == AE_FIM_ImportFlavorRef_NONE)
-					return err;
-
-				AE_FIM_ImportCallbacks importCallbacks = {};
-				importCallbacks.refcon = nullptr;
-				importCallbacks.import_cb = AEGP_FileImportCallbackHandler;
-				importCallbacks.verify_cb = AEGP_FileVerifyCallbackHandler;
-
-				ERR(suites.FIMSuite3()->AEGP_RegisterImportFlavorImportCallbacks(importFlavorRef, AE_FIM_ImportFlag_COMP, &importCallbacks));
-			}
+			ERR(suites.FIMSuite3()->AEGP_RegisterImportFlavorImportCallbacks(importFlavorRef, AE_FIM_ImportFlag_COMP, &importCallbacks));
 			return err;
 		}
 	}
@@ -81,6 +85,6 @@ A_Err EntryPointFunc(SPBasicSuite* pica_basicP, A_long major_versionL, A_long mi
 	AetPlugin::GlobalBasicPicaSuite = pica_basicP;
 	AetPlugin::GlobalPluginID = aegp_plugin_id;
 
-	const auto error = AetPlugin::RegisterAetFileType();
+	const auto error = AetPlugin::RegisterAetSetFileType();
 	return error;
 }
