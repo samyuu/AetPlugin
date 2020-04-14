d050be37f07ac827638129c79a96893b3357daa3
samyuu <samyuu@users.noreply.github.com>
2020-04-15 01:13:42 +0200

    FormatUtil implement FormatFlags

--- src/FormatUtil.h
+++ src/FormatUtil.h
@@ -76,4 +76,22 @@ namespace FormatUtil
 		std::replace(result.begin(), result.end(), ' ', '_');
 		return result;
 	}
+
+	template <typename FlagsType, typename FlagsArray>
+	inline std::string FormatFlags(const FlagsType flags, const FlagsArray& flagsToCheck)
+	{
+		static constexpr std::string_view separator = " | ";
+
+		std::string result;
+		{
+			for (const auto&[mask, name] : flagsToCheck)
+				if (flags & mask) { result += name; result += separator; }
+
+			if (!result.empty())
+				result[result.size() - separator.size()] = '\0';
+			else
+				result += "None";
+		}
+		return result;
+	}
 }
