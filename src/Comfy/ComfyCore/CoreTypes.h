53fcf73dd56d3dae43b6f517d8c87b5a10c56b90
samyuu <samyuu@users.noreply.github.com>
2020-06-06 07:27:49 +0200

    Start removing dependency source code

--- src/Comfy/ComfyCore/CoreTypes.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-#include <array>
-#include <vector>
-#include "Core/Algorithm.h"
--- src/Comfy/ComfyCore/Misc/ImageHelper.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Types.h"
-#include "CoreTypes.h"
-
-namespace Comfy::Utilities
-{
-	void ReadImage(std::string_view filePath, ivec2& outSize, UniquePtr<uint8_t[]>& outRGBAPixels);
-	void WritePNG(std::string_view filePath, ivec2 size, const void* rgbaPixels);
-}
